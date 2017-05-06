0e6f73230823abf3211a60c437423905392741ca
Balachandar-R <balachandar.raju@gmail.com>
2017-05-06 18:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     #zone = parameters.get("shipping-zone")
-    months = parameters.get("amount")
+    months = parameters.get("amount.amount")
     #months = 12
-    princ_amt = parameters.get("duration")
+    princ_amt = parameters.get("duration.amount")
     #princ_amt = 20000
     interest_rate = 0.13
     
